f9a05c69a548ee1a3e5cbfa0b3010ba870d22a8a
radenlmantuano <newcodeondablock@gmail.com>
2023-08-21 22:13:09 -0700

    added about me body

--- src/components/AboutMeModalBody.jsx
+++ src/components/AboutMeModalBody.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const AboutMeModalBody = () => {
   return (
-    <div>
-      HELLO
+    <div className='about-me-body-container'>
+      <div className='about-me-body-paragraph'>
+        <p style={{textIndent: '25px'}}>
+          My name is Raden! Yup.. pronounced just like in the movie, "Mortal Kombat". 
+          I'm a seasoned Software Engineer driven by a passion for innovation, physical well-being, complex problem-solving, and the revolutionary domains of blockchain and cryptocurrency technologies. Boasting an extensive track record of over 8 years in the field, supplemented by 2 years of hands-on involvement in a high-impact production setting, I possess the innate capacity to convert my visions into reality. My adeptness at rapid assimilation, coupled with an unwavering commitment to collaborative teamwork, effective communication, and outcome-oriented methodologies, sets the tone for my professional journey.
+          My appetite for tackling challenges serves as a catalyst for my personal growth and extends to aiding others in their quests. As of now, my primary focus revolves around the development of front-end web applications, yet my proficiency extends across the full spectrum of the development landscape. My toolkit includes a mastery of:
+          Cutting-edge frameworks such as Vue and ReactJs
+          Languages spanning JavaScript, TypeScript, Web3Js, Solidity, and GraphQL (which currently occupies my attention)
+          The versatile MERN stack encompassing React, MongoDb, Mongoose, Express, and NodeJs
+          Python expertise covering Flask and MySQL
+          Proficiency in Java, particularly in the Springboot Data JPA framework and MySQL
+          My journey is marked by successfully navigating intricate production environments, where I've harnessed my prowess in Vue, React, JavaScript, TypeScript, Object-Oriented Programming, Functional Programming, Web3JS, GraphQL, MySQL, and MongoDB. As I continue to pave my path in the ever-evolving software landscape, I remain dedicated to unraveling complexities and reshaping possibilities.
+          <br></br>
+          <br></br>
+            Warm regards,
+          <br></br>
+            Raden Mantuano
+        </p>
+      </div>
     </div>
   )
 }
