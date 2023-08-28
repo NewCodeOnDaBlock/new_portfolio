8d745b8c6b0e977c1f6c9f5e6c4b131c90168281
radenlmantuano <newcodeondablock@gmail.com>
2023-08-28 11:40:39 -0700

    portfolio addition

--- src/components/PortfolioModalBody.jsx
+++ src/components/PortfolioModalBody.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import MEW from '../assets/MEW.png'
 import DonkOne from '../assets/donk_one.png'
+import SongPlayer from '../assets/song_player_app.png'
+import GitHub from '../assets/github.png'
+
 
 
 const PortfolioModalBody = () => {
@@ -20,7 +23,7 @@ const PortfolioModalBody = () => {
                 <img
                   className='portfolio-image'
                   src={DonkOne}
-                  alt='mew'
+                  alt='donk-one'
                 />
                 <h3>
                   <a href="https://www.donkswap.com/" target="_blank" rel="noreferrer">
@@ -28,7 +31,33 @@ const PortfolioModalBody = () => {
                   </a>
                 </h3>
                 <p>ReactJs | Web3js | EtherJs | SASS | TypeScript | </p>    
-              </div>   
+            </div>   
+            <div className="project-container">
+                <img
+                  className='portfolio-image'
+                  src={SongPlayer}
+                  alt='song-player'
+                />
+                <h3>
+                  <a href="https://main--symphonious-druid-3da981.netlify.app/" target="_blank" rel="noreferrer">
+                    Song Player App
+                  </a>
+                </h3>
+                <p>ReactJs | SASS | </p>    
+            </div>   
+            <div className="project-container">
+                <img
+                  className='portfolio-image'
+                  src={GitHub}
+                  alt='git-hub'
+                />
+                <h3>
+                  <a href="https://github.com/NewCodeOnDaBlock?tab=overview&from=2022-12-01&to=2022-12-31" target="_blank" rel="noreferrer">
+                    See My Github for more projects...
+                  </a>
+                </h3>
+                <p> Full Stack Software Engineer Experienced in navigating complex production environments in #ReactJs #Vue #JavaScript #TypeScript #Web3Js #EtherJs </p>    
+            </div>   
     </div>
   )
 }
